d7644771bd956957a3c2327cb5fc1a4edf553295
Erik Thompson <122100036+assinshells@users.noreply.github.com>
2025-12-13 15:15:07 +0200

    modified:   frontend/src/pages/chat/ui/ChatPage.jsx

--- frontend/src/pages/chat/ui/ChatPage.jsx
+++ frontend/src/pages/chat/ui/ChatPage.jsx
@@ -41,6 +41,11 @@ export const ChatPage = () => {
                   <i className="bi bi-person"></i>
                 </a>
               </li>
+              <li className="nav-item" title="">
+                <a className="nav-link" href="#" data-bs-target="#settingModal">
+                  <i className="bi bi-gear"></i>
+                </a>
+              </li>
 
               {/* Mobile Dropdown */}
               <li className="nav-item dropdown profile-user-dropdown d-inline-block d-lg-none">
@@ -196,6 +201,43 @@ export const ChatPage = () => {
                 <strong>Email:</strong> {user.email}
               </p>
             )}
+          </div>
+        </div>
+      </div>
+      {/* Setting Modal */}
+      <div
+        class="modal fade"
+        id="settingModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Settings
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
           </div>
         </div>
       </div>
