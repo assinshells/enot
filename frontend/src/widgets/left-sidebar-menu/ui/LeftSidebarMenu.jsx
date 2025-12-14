dfad77c83259076896e154f0ced49e6af9684de7
Erik Thompson <122100036+assinshells@users.noreply.github.com>
2025-12-14 18:23:23 +0200

    modified:   frontend/src/widgets/left-sidebar-menu/ui/LeftSidebarMenu.jsx

--- frontend/src/widgets/left-sidebar-menu/ui/LeftSidebarMenu.jsx
+++ frontend/src/widgets/left-sidebar-menu/ui/LeftSidebarMenu.jsx
@@ -4,6 +4,7 @@
  */
 import { useAuth } from "@/shared/lib/hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import chatLogo from "../../../assets/react.svg";
 import "./LeftSidebarMenu.css";
 
 export const LeftSidebarMenu = ({ onOpenSettings }) => {
@@ -21,69 +22,101 @@ export const LeftSidebarMenu = ({ onOpenSettings }) => {
   };
 
   return (
-    <div className="side-menu flex-lg-column me-lg-1 ms-lg-0">
-      <div className="flex-lg-column my-auto">
-        <ul className="nav side-menu-nav justify-content-center">
-          <li className="nav-item">
-            <button
-              className="nav-link"
-              onClick={handleSettingsClick}
-              title="Настройки"
-              type="button"
-            >
-              <i className="bi bi-gear"></i>
-            </button>
-          </li>
+    <>
+      <div className="side-menu flex-lg-column me-lg-1 ms-lg-0">
+        <div className="navbar-brand-box">
+          <a href="#" className="logo logo-dark">
+            <span className="logo-sm">
+              <img src={chatLogo} alt="" height="30" />
+            </span>
+          </a>
 
-          {/* Mobile Dropdown */}
-          <li className="nav-item dropdown profile-user-dropdown d-inline-block d-lg-none">
-            <button
-              className="nav-link dropdown-toggle no-caret"
-              type="button"
-              data-bs-toggle="dropdown"
-            >
-              <i className="bi bi-list"></i>
-            </button>
-            <div className="dropdown-menu">
-              <button className="dropdown-item" onClick={handleSettingsClick}>
-                <i className="bi bi-gear float-end text-muted"></i>
-                Настройки
+          <a href="#" className="logo logo-light">
+            <span className="logo-sm">
+              <img src={chatLogo} alt="" height="30" />
+            </span>
+          </a>
+        </div>
+        <div className="flex-lg-column my-auto">
+          <ul
+            className="nav nav-pills side-menu-nav justify-content-center"
+            role="tablist"
+          >
+            <li className="nav-item">
+              <button
+                className="nav-link"
+                onClick={handleSettingsClick}
+                title="Настройки"
+                type="button"
+              >
+                <i className="bi bi-gear"></i>
               </button>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item" onClick={handleLogout}>
-                <i className="bi bi-box-arrow-right float-end text-muted"></i>
-                Выход
-              </button>
-            </div>
-          </li>
-        </ul>
-      </div>
+            </li>
 
-      {/* Desktop Bottom Menu */}
-      <div className="flex-lg-column d-none d-lg-block">
-        <ul className="nav side-menu-nav justify-content-center">
-          <li className="nav-item btn-group dropup profile-user-dropdown">
-            <button
-              className="nav-link dropdown-toggle no-caret"
-              type="button"
-              data-bs-toggle="dropdown"
-            >
-              <i className="bi bi-list"></i>
-            </button>
-            <div className="dropdown-menu">
-              <button className="dropdown-item" onClick={handleSettingsClick}>
-                <i className="bi bi-gear float-end text-muted"></i>
-                Настройки
-              </button>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item" onClick={handleLogout}>
-                <i className="bi bi-box-arrow-right float-end text-muted"></i>
-                Выход
-              </button>
-            </div>
-          </li>
-        </ul>
+            {/* Mobile Dropdown */}
+            <li className="nav-item dropdown profile-user-dropdown d-inline-block d-lg-none">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="bi bi-list"></i>
+              </a>
+              <div className="dropdown-menu">
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={handleSettingsClick}
+                >
+                  Setting <i className="bi bi-gear float-end text-muted"></i>
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#" onClick={handleLogout}>
+                  Log out
+                  <i className="bi bi-box-arrow-right float-end text-muted"></i>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        {/* Desktop Bottom Menu */}
+        <div className="flex-lg-column d-none d-lg-block">
+          <ul className="nav side-menu-nav justify-content-center">
+            <li className="nav-item btn-group dropup profile-user-dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="bi bi-list"></i>
+              </a>
+
+              <div className="dropdown-menu">
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={handleSettingsClick}
+                >
+                  Setting
+                  <i className="bi bi-gear float-end text-muted"></i>
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#" onClick={handleLogout}>
+                  Log out
+                  <i className="bi bi-box-arrow-right float-end text-muted"></i>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
