7083a6e03bad9b61dcdbae1968a3f7279109b395
Erik Thompson <122100036+assinshells@users.noreply.github.com>
2025-12-19 23:59:20 +0200

    modified:   frontend/src/app/styles/app.css 	modified:   frontend/src/pages/email-confirmation/ui/EmailConfirmationPage.jsx 	modified:   frontend/src/pages/forgot-password/ui/ForgotPasswordPage.jsx 	modified:   frontend/src/pages/reset-password/ui/ResetPasswordPage.jsx

--- frontend/src/pages/email-confirmation/ui/EmailConfirmationPage.jsx
+++ frontend/src/pages/email-confirmation/ui/EmailConfirmationPage.jsx
@@ -3,24 +3,25 @@ import { EmailConfirmationForm } from "@/features/auth";
 export const EmailConfirmationPage = () => {
   return (
     <>
-      <div className="account-pages my-5 pt-sm-5">
-        <div className="container">
-          <div className="justify-content-center row">
-            <div className="col-md-8 col-lg-6 col-xl-5">
-              <div className="text-center mb-4">
-                <a class="auth-logo mb-5 d-block" href="/">
-                  <img src="" alt="" height="30" class="logo logo-dark" />
-                  <img src="" alt="" height="30" class="logo logo-light" />
-                </a>
-                <h1>Chat App</h1>
-                <p className="text-muted mb-4">
-                  Создайте аккаунт для входа в чат
-                </p>
+      <div class="account-pages my-5 pt-sm-5">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-5">
+              <div class="text-center mb-4">
+                <h2 className="auth-logo mb-2">Чат Stuff</h2>
+                <p className="auth-subtext mb-3">тут щось є. начебто</p>
               </div>
               <EmailConfirmationForm />
             </div>
           </div>
         </div>
+      </div>
+      <div className="mt-5 text-center auth-footer">
+        <p>Новый пользователь? Просто введите никнейм и пароль</p>
+        <p>
+          © 2025 Chatvia. Crafted with{" "}
+          <i className="bi bi-heart-fill text-danger"></i> by Themesbrand
+        </p>
       </div>
     </>
   );
